9dc71bdbe147dad18015776057d201d3b43dc6d0
MMCore <marko.soccer@gmail.com>
2016-12-05 17:13:32 +1100

    Moved MYID to config.js

--- bot.js
+++ bot.js
@@ -1,9 +1,8 @@
 var Twit = require('twit')
 var fs = require('fs')
 var config = require('./config.js')
-var T = new Twit(config)
+var T = new Twit(config.access)
 var stream = T.stream('user')
-var MYID = '' //Enter Your ID Here e.g: '5425246167376373'
 
 //Dictionary Storing Id's(Keys) and Times(Values) When They Followed You
 var followArray = JSON.parse(fs.readFileSync("dict.txt").toString())
@@ -29,7 +28,7 @@ setInterval(function removeUnfollower(){
 
 //Follows Anyone Who Follows You And Stores Them In the followDict
 function followPerson(event){
-  if(event.source.id_str != MYID){    //Ignores when I follow people
+  if(event.source.id_str != config.MYID.ID){    //Ignores when I follow people
     T.post('friendships/create', { id: event.source.id_str }, function(err, data, response) {
       if(err){
         console.log(err)
@@ -39,12 +38,12 @@ function followPerson(event){
       }
     })
   }else{
-    console.log('I followed!')
+    console.log('I followed')
     followArray[event.target.id_str] = Date.now()
   }
   fs.writeFile('dict.txt',JSON.stringify(followArray), function (err) {
     if (err) throw err;
-    console.log('Dictionary Updated!');
+    console.log('Dictionary Updated')
   })
 }
 
@@ -56,8 +55,8 @@ function unfollowPerson(id_str){
       delete followArray[id_str]
     }
     fs.writeFile('dict.txt',JSON.stringify(followArray), function (err) {
-      if (err) throw err;
-      console.log('Dictionary Updated!');
+      if (err) throw err
+      console.log('Dictionary Updated!')
     })
   })
 }
--- config.js
+++ config.js
@@ -1,6 +1,10 @@
-module.exports = {
+module.exports.access = {
   consumer_key: '',	    //Enter Your consumer_key
   consumer_secret: '',	    //Enter Your consumer_secret
   access_token: '',	    //Enter Your access_token
-  access_token_secret: ''   //Enter Your access_token_secret
+  access_token_secret: '',   //Enter Your access_token_secret
+}
+
+module.exports.ID = {
+  MYID: ''   //Enter Your ID Here e.g: '774768628611395713'
 }
